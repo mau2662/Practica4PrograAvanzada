8ccf14955d5f5b3e31ffbabd7d84e70676bb24e0
Mauricio <vargas7384@gmail.com>
2023-12-06 17:36:50 -0600

    FirstCommit

---
+++ Practica4Web/Practica4Web/Controllers/ProductoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica4Web.Controllers
+{
+    public class ProductoController : Controller
+
+
+    {
+        [HttpGet]
+        public IActionResult ConsultarProducto()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult RegistrarProducto()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practica4Web/Practica4Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica4Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
