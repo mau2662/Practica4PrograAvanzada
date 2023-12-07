32f351362b1633e525305f3e8ed0c221d594dad6
mau2662 <vargas7384@gmail.com>
2023-12-06 18:42:16 -0600

    Update MVC

---
+++ Practica4Web/Practica4Web/Entities/ProductoEnt.cs
@@ -0,0 +1,18 @@
+﻿namespace Practica4Web.Entities
+{
+    public class ProductoEnt
+    {
+        public long Id_Abono { get; set; }
+        public decimal Monto { get; set; }
+        public DateTime Fecha { get; set; }
+        public long Id_Compra { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public decimal Saldo { get; set; }
+
+        public string Descripcion { get; set; } = string.Empty;
+
+        public string Estado { get; set; } = string.Empty;
+    }
+}
